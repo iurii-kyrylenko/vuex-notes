aea0e158ac85ecfb6666741d818508d0aa27d34f
iurii-kyrylenko <yuriproject@gmail.com>
2017-02-26 16:29:45 +0200

    An initial commit.

---
+++ src/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div class="well test">TEST</div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style src="bootstrap/dist/css/bootstrap.css">
+</style>
+
+<style src="../assets/styles.css">
+</style>
+
